06f4b68f2a0b98a983dd581c8f4f168ef4e56f23
Camilo Puentes <puentes.camilo@gmail.com>
2020-07-10 14:39:46 -0500

    agregamos una funcion para calcular el cambio de pesos mexicanos a pesos colombianos

--- calculadora_de_divisas.py
+++ calculadora_de_divisas.py
@@ -1,10 +1,21 @@
 
+def foreign_exchange_calculator(ammount):
+    mex_to_col_rate = 145.97
+
+    return mex_to_col_rate * ammount
+
+
 def run():
     print('CALCULADORA DE DIVISAS')
     print('Convierte pesos mexicanos a pesos colombianos')
     print('')
 
     ammount = float(input('Ingresa la cantidad de pesos mexicanos que quieres convertir: '))
+
+    result = foreign_exchange_calculator(ammount)
+
+    print('${} Pesos mexicanos son ${} pesos colombianos'.format(ammount, result))
+    print('')
 
 if __name__ == '__main__':
     run()
